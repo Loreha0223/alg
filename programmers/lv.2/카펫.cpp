a1844c361a9a1d1db7ba8e435e7fe6f3b769ba40
Loreha <rhdlwmal123@naver.com>
2022-11-18 18:55:04 +0900

    221118
    day 5

--- programmers/lv.2/카펫.cpp
+++ programmers/lv.2/카펫.cpp
@@ -24,27 +24,5 @@ int main() {
 		}
 		if (flag == 0) { x = yellow + 2, y = 3; }
 	}
-	
-
-
-
-
-
-	/*if (yellow == 1) { x=3, y = 3; }
-	else if (yellow == 2) { x = 4, y = 3; }
-	else {
-		int i;
-		int min = 1000000;
-		for (i = (yellow/2)+1; i >= 2; i--) {
-			int dx = i + 2;
-			int dy = yellow / i + 2;
-			if (yellow % i == 0 && min>abs(dx-dy) &&(dx>=dy)) {
-				x = dx;
-				y = dy;
-				min = x - y;
-			}
-		}
-		if (min == 1000000) { x = yellow + 2, y = 3; }
-	}*/
 	cout << x << ' ' << y;
 }
