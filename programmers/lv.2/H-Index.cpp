b7ebe050fad98d7d20e8bba5a933407f9562eb80
Loreha <rhdlwmal123@naver.com>
2022-11-19 18:19:37 +0900

    221119
    day 6

---
+++ programmers/lv.2/H-Index.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	vector<int> citations;
+	int n=0;
+	while (n>-1) {
+		cin >> n;
+		citations.push_back(n);
+	}
+	sort(citations.begin(), citations.end(),greater<int>());
+	int sum = 0;
+	for (int i = 0; i < citations.size(); i++) {
+		if (i + 1 <= citations[i])sum++;
+		else break;
+	}
+	cout << sum;
+}
