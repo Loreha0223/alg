116e695e9d131945920a8307cf6215a9c1dc0b10
Loreha <rhdlwmal123@naver.com>
2022-11-19 18:23:44 +0900

    221119
    day 6

--- programmers/lv.2/H-Index.cpp
+++ programmers/lv.2/H-Index.cpp
@@ -12,6 +12,8 @@ int main() {
 		cin >> n;
 		citations.push_back(n);
 	}
+	citations.pop_back();
+
 	sort(citations.begin(), citations.end(),greater<int>());
 	int sum = 0;
 	for (int i = 0; i < citations.size(); i++) {
